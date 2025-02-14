811a945395bfed846e3ed69194bcae17f4e4528e
Huameisoft <Willie.Yao@huameisoft.cn>
2025-02-14 13:17:53 +0800

    Commit by Willie Yao

--- InboundCrmIntegration/InboundCrmIntegration/Program.cs
+++ InboundCrmIntegration/InboundCrmIntegration/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.Xrm.Sdk;
+﻿using Microsoft.Xrm.Sdk;
 using System;
 using Microsoft.Xrm.Sdk.Query;
 using Microsoft.Xrm.Tooling.Connector;
@@ -13,8 +13,8 @@ using System.DirectoryServices.ActiveDirectory;
 using Microsoft.Xrm.Sdk.Messages;
 using Microsoft.Xrm.Sdk.Metadata;
 using System.Web.Services.Description;
- 
-namespace ToCRM
+
+namespace InboundCrmIntegration
 {
     internal class Program
     {
@@ -23,18 +23,18 @@ namespace ToCRM
             ServicePointManager.ServerCertificateValidationCallback += ValidateCertificate;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
             //string connectString = "Url=https://crmdev.huamei.com/XRMServices/2011/Organization.svc; Domain=huamei.com; Username=willie.Yao@huamei.com; Password=pass@word123";
- 
+
             // 🔹 声明并初始化 clientCredentials
             ClientCredentials clientCredentials = new ClientCredentials();
- 
+
             clientCredentials.UserName.UserName = "willie.Yao" + "@" + "huamei.com";
             clientCredentials.UserName.Password = "pass@word123";
             //clientCredentials.Windows.ClientCredential = new System.Net.NetworkCredential("willie.Yao", "pass@word123", "huamei.com");
             IOrganizationService organizationServiceProxy = new OrganizationServiceProxy(new Uri("https://crmdev.huamei.com/XRMServices/2011/Organization.svc"), null, clientCredentials, null);
- 
+
             // var entitySingle = organizationServiceProxy.Retrieve("account", new Guid("e9161bbf-5ae8-ef11-811f-00155d01060d"), new ColumnSet("accountid"));
             // Console.WriteLine($"账户名称：{entitySingle["name"]}");
- 
+
             // 一、下拉所有可用的实体
             /* RetrieveAllEntitiesRequest request = new RetrieveAllEntitiesRequest()
             //{
@@ -47,10 +47,10 @@ namespace ToCRM
             //{
             //    Console.WriteLine($"Entity Logical Name: {entityMetadata.LogicalName}");
             }*/
- 
+
             // 二、给存在的contact创建一条新记录
             Entity entityCreate = new Entity("account");  // 替换为你自定义实体的逻辑名称
- 
+
             // 设置实体字段的值
             // entityCreate["name"] = "";  // 例如设置名称字段 该XXid字段不能被赋值 是由crm系统赋值
             entityCreate["name"] = "Mina Xu";  // 设置描述字段
@@ -60,18 +60,18 @@ namespace ToCRM
             entityCreate["createdon"] = DateTime.Now; // 自定义日期字段
             // 调用Create方法将记录保存到CRM中
             Guid entityId = organizationServiceProxy.Create(entityCreate);
- 
+
             // 打印创建的记录ID
             Console.WriteLine($"新记录的ID是：{entityId}");
             //新记录的ID是：3cad7804-dae9-ef11-811f-00155d01060d
 
- 
- 
+
+
             //三、删除,物理删除
             /*
             organizationServiceProxy.Delete("account", new Guid("3cad7804-dae9-ef11-811f-00155d01060d"));
             */
- 
+
             //四、更新, 在更新时建议使用这种方式, 如果将原有的数据全部查出来再修改部分字段, 会导致没有修改的字段也会被update, 可能会触发某些插件、流程
             /*
             var entityUpdate = new Entity("account", new Guid("3cad7804-dae9-ef11-811f-00155d01060d"));//必须填写被更新的id
@@ -79,7 +79,7 @@ namespace ToCRM
             //...更新的赋值和新增一致, 不赘述
             organizationServiceProxy.Update(entityUpdate);
             */
- 
+
             //五、查询单条: Retrieve 此方法没有找到数据会直接引发异常慎用(tableName With Id = xxx Does Not Exist), 类似.First()
             /*
             var entitySingle = organizationServiceProxy.Retrieve("account", new Guid("3cad7804-dae9-ef11-811f-00155d01060d"), new ColumnSet("name", "createdon"));
@@ -103,9 +103,7 @@ namespace ToCRM
                 Console.WriteLine("该记录没有 'createdon' 字段的值");
             }
             */
- 
         }
- 
         private static bool ValidateCertificate(object sender, X509Certificate cert, X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
         {
             return true;
