6355edba7d90bf128c71ef56f09baf032c52668d
Willie Yao <qq1529254181@163.com>
2025-04-07 21:56:44 +0800

    feat(database-design): Create basic controller class for select data from sql server.

---
+++ InboundCrmIntegration/DealGeinieCrmService/Controllers/EnterpriseController.cs
@@ -0,0 +1,47 @@
+﻿using DealGeinieCrmService.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+using System.Web.Http;
+
+namespace DealGeinieCrmService.Controllers
+{
+    [RoutePrefix("api/enterprise")]
+    public class EnterpriseController : ApiController
+    {
+        private EnterpriseContext db = new EnterpriseContext();
+
+        [Route("{id}")]
+        [HttpGet]
+        public IQueryable<EnterpriseInfo> GetEnterprises()
+        {
+            return db.Enterprises;
+        }
+
+        [Route("{id}")]
+        [HttpPost]
+        [ResponseType(typeof(EnterpriseInfo))]
+        public async Task<IHttpActionResult> PostEnterprise(EnterpriseInfo enterprise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Enterprises.Add(enterprise);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi",
+                new { id = enterprise.UnifiedSocialCreditCode }, enterprise);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
